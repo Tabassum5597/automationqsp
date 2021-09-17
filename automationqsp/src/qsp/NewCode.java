926ec8f11c8c7c327233521af32e47eaa0bfc650
Tabassum5597 <90815758+Tabassum5597@users.noreply.github.com>
2021-09-17 09:53:29 +0530

    jnjnn

--- automationqsp/src/qsp/NewCode.java
+++ automationqsp/src/qsp/NewCode.java
@@ -4,7 +4,7 @@ public class NewCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("to much HARD WORK");
+System.out.println("123");
 	}
 
 }
